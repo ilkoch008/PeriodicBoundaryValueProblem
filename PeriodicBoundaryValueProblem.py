3fc3ddb439f63858a736e0d7f306362db197d4e0
ilkoch008 <ilkoch008@gmail.com>
2020-04-24 13:12:17 +0300

    solution of system

--- PeriodicBoundaryValueProblem.py
+++ PeriodicBoundaryValueProblem.py
@@ -37,8 +37,10 @@ for i in range(1, matrix.__len__()):
 matrix[0][N-1] = 1
 matrix[N-1][0] = 1
 
+f = numpy.array(f)
+
 matrix_np = numpy.array(matrix)
-solution = numpy.linalg.solve(matrix_np, f)
+solution = numpy.linalg.solve(matrix_np, math.pow(h, 2) * f)
 
 matrix_B = [0.0] * (N - 1)
 for i in range(0, matrix_B.__len__()):
@@ -55,9 +57,48 @@ for i in range(0, N - 1):
 column_G = [0.0] * (N - 1)
 column_G[0] = -1
 column_G[N-2] = -1
+column_F = numpy.array(column_F)
+column_G = numpy.array(column_G)
 
-x_1 = misc.solveTriMatrix(matrix_B, column_F)
+x_1 = misc.solveTriMatrix(matrix_B, math.pow(h, 2) * column_F)
+
 x_2 = misc.solveTriMatrix(matrix_B, column_G)
 
+x_N_1 = (misc.f(1) * math.pow(h, 2) - x_1[0] - x_1[N-2])/(matrix[N-1][N-1] + x_2[N-2] + x_2[0])
+
+print(x_N_1)
+
+my_pre_solution = [N-1] * (N-1)
+for i in range(0, N - 1):
+    my_pre_solution[i] = x_1[i] + x_N_1 * x_2[i]
+
+my_solution = [0.0] * N
+
+for i in range(0, N - 1):
+    my_solution[i] = my_pre_solution[i]
+
+my_solution[N-1] = x_N_1
+
+plt.plot(t_lol, x_1)
+plt.grid(True)
+plt.minorticks_on()
+plt.grid(which='minor', linestyle=':')
+plt.show()
+
 plt.plot(t_lol, x_2)
+plt.grid(True)
+plt.minorticks_on()
+plt.grid(which='minor', linestyle=':')
+plt.show()
+
+plt.plot(t, my_solution)
+plt.grid(True)
+plt.minorticks_on()
+plt.grid(which='minor', linestyle=':')
+plt.show()
+
+plt.plot(t, solution)
+plt.grid(True)
+plt.minorticks_on()
+plt.grid(which='minor', linestyle=':')
 plt.show()
--- PeriodicBoundaryValueProblemMisc.py
+++ PeriodicBoundaryValueProblemMisc.py
@@ -16,22 +16,33 @@ def b(n, h):
 
 
 def solveTriMatrix(matrix, column):
+    loc_matrix = [0] * len(matrix)
+    for i in range(0, len(matrix)):
+        loc_matrix[i] = [0.0] * len(matrix)
+
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix)):
+            loc_matrix[i][j] = matrix[i][j]
+
     solution = [0.0] * len(column)
     for i in range(0, len(column)):
         solution[i] = column[i]
 
-    for i in range(0, len(matrix) - 1):
-        k = matrix[i+1][i] / matrix[i][i]
-        for j in range(0, len(matrix)):
-            matrix[i+1][j] = matrix[i+1][j] - matrix[i][j] * k
+    for i in range(0, len(loc_matrix) - 1):
+        k = loc_matrix[i+1][i] / loc_matrix[i][i]
+        for j in range(0, len(loc_matrix)):
+            loc_matrix[i+1][j] = loc_matrix[i+1][j] - loc_matrix[i][j] * k
 
         solution[i+1] = solution[i+1] - solution[i] * k
 
-    for i in range(len(matrix) - 2, 0):
-        k = matrix[i][i+1] / matrix[i+1][i+1]
-        for j in range(0, len(matrix)):
-            matrix[i][j] = matrix[i][j] - matrix[i+1][j] * k
+    for i in range(len(loc_matrix) - 2, -1, -1):
+        k = loc_matrix[i][i+1] / loc_matrix[i+1][i+1]
+        for j in range(0, len(loc_matrix)):
+            loc_matrix[i][j] = loc_matrix[i][j] - loc_matrix[i+1][j] * k
 
         solution[i] = solution[i] - solution[i+1] * k
 
-    return solution
+    for i in range(0, len(solution)):
+        solution[i] = solution[i] / loc_matrix[i][i]
+
+    return numpy.array(solution)
